b755e5e90347ca227ead35cfa28aed3c17f7e1a4
eva990417744 <990417744@qq.com>
2018-04-10 15:10:47 +0800

    Update docker_mangement.sh

--- docker_mangement.sh
+++ docker_mangement.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install -y docker
+#!/bin/sh
+curl -sSL https://get.daocloud.io/docker > install.sh
+sh install.sh
 sudo docker pull sumeragibi/docker_mangement
 sudo docker run -p 5001:5001 -v /var/run/docker.sock:/var/run/docker.sock --name "docker_mangement" -d -t sumeragibi/docker_mangement /bin/bash start.sh
